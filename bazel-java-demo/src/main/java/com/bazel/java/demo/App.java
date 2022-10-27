3e2d62901d52509dc095ec99f3c5b6d59b7d16cd
Wenjuan Li <wenjuan.li@compass.com>
2022-10-27 11:27:54 +0800

    compile and run project with Bazel

---
+++ bazel-java-demo/src/main/java/com/bazel/java/demo/App.java
@@ -0,0 +1,9 @@
+package com.bazel.java.demo;
+
+import com.bazel.java.demo.greeting.Greeting;
+
+public class App {
+  public static void main(String[] args) {
+    Greeting.sayHi();
+  }
+}
---
+++ bazel-java-demo/src/main/java/com/bazel/java/demo/runner/Runner.java
@@ -0,0 +1,9 @@
+package com.bazel.java.demo.runner;
+
+import com.bazel.java.demo.greeting.Greeting;
+
+public class Runner {
+    public static void main(String args[]) {
+        Greeting.sayHi();
+    }
+}
