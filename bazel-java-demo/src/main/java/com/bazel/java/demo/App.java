eece74cd921925ba32b22978ef0d1cbc639b6bd0
Wenjuan Li <wenjuan.li@compass.com>
2022-10-31 11:48:21 +0800

    rename DemoModule to GuiceModule

--- bazel-java-demo/src/main/java/com/bazel/java/demo/App.java
+++ bazel-java-demo/src/main/java/com/bazel/java/demo/App.java
@@ -1,13 +1,13 @@
 package com.bazel.java.demo;
 
-import com.bazel.java.demo.guice.module.DemoModule;
+import com.bazel.java.demo.guice.module.GuiceModule;
 import com.bazel.java.demo.guice.Greeter;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 
 public class App {
   public static void main(String[] args) {
-    Injector injector = Guice.createInjector(new DemoModule());
+    Injector injector = Guice.createInjector(new GuiceModule());
     Greeter greeter = injector.getInstance(Greeter.class);
     greeter.sayHello();
   }
--- bazel-java-demo/src/main/java/com/bazel/java/demo/guice/module/GuiceModule.java
+++ bazel-java-demo/src/main/java/com/bazel/java/demo/guice/module/GuiceModule.java
@@ -5,7 +5,7 @@ import com.bazel.java.demo.guice.annotation.Message;
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
 
-public class DemoModule extends AbstractModule {
+public class GuiceModule extends AbstractModule {
   @Provides
   @Count
   static Integer provideCount() {
